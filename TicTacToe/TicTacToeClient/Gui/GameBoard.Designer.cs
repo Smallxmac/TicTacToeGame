c43db1baf3c1b18819c8e179639f1bad66aedc4c
Michael <smallxmac@gmail.com>
2015-09-19 17:59:26 -0500

    First push!

---
+++ TicTacToe/TicTacToeClient/Gui/GameBoard.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace TicTacToeClient.Gui
+{
+    partial class GameBoard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gamePanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // gamePanel
+            // 
+            this.gamePanel.BackColor = System.Drawing.SystemColors.Control;
+            this.gamePanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.gamePanel.Location = new System.Drawing.Point(7, 12);
+            this.gamePanel.Name = "gamePanel";
+            this.gamePanel.Size = new System.Drawing.Size(600, 600);
+            this.gamePanel.TabIndex = 0;
+            this.gamePanel.Paint += new System.Windows.Forms.PaintEventHandler(this.gamePanel_Paint);
+            this.gamePanel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.gamePanel_MouseClick);
+            // 
+            // GameBoard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(613, 617);
+            this.Controls.Add(this.gamePanel);
+            this.Name = "GameBoard";
+            this.Text = "TicTacToe GameBoard";
+            this.Load += new System.EventHandler(this.GameBoard_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel gamePanel;
+    }
+}
---
+++ TicTacToe/TicTacToeClient/Gui/GameBoard.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Xsl;
+
+namespace TicTacToeClient.Gui
+{
+    public partial class GameBoard : Form
+    {
+
+        private Graphics _graphics;
+        private Pen _pen = new Pen(Color.Black);
+        private Brush _brush = new SolidBrush(Color.Black);
+        private SpaceTypes[,] _fieldSpaces = new SpaceTypes[3,3];
+
+        public GameBoard()
+        {
+            InitializeComponent();
+        }
+
+        private void GameBoard_Load(object sender, EventArgs e)
+        {
+            ClearField();
+        }
+
+        private void DrawField()
+        {
+            #region Rectangles
+
+            Rectangle firstVerticalRectangle = new Rectangle(190, 0, 15, 600);
+            Rectangle secondVerticalRectangle = new Rectangle(395, 0, 15, 600);
+            Rectangle firstHorizontalRectangle = new Rectangle(0, 190, 600, 15);
+            Rectangle secondHorizontalRectangle = new Rectangle(0, 395, 600, 15);
+            Rectangle[] allRectangles =
+            {
+                firstVerticalRectangle, secondVerticalRectangle,
+                firstHorizontalRectangle, secondHorizontalRectangle
+            };
+
+            #endregion
+
+            #region PlaySpaces
+
+            for (int i = 0; i < 3; i++)
+            {
+                int x = 0, y = 0;
+                for (int j = 0; j < 3; j++)
+                {
+                    if (i == 1)
+                        x = 205;
+                    if (i == 2)
+                        x = 410;
+                    if (j == 1)
+                        y = 205;
+                    if (j == 2)
+                        y = 410;
+
+                    switch (_fieldSpaces[i,j])
+                    {
+                        case SpaceTypes.Open:
+                            _graphics.FillRectangle(new SolidBrush(Color.Gold), x, y, 190, 190);
+                            break;
+                        case SpaceTypes.O:
+                            _graphics.FillRectangle(new SolidBrush(Color.Red), x, y, 190, 190);
+                            break;
+
+                    }
+                }
+            }
+
+
+            #endregion
+
+            _graphics.FillRectangles(_brush, allRectangles);
+
+        }
+
+        private void ClearField()
+        {
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    _fieldSpaces[i,j] = SpaceTypes.Open;
+        }
+
+        private void gamePanel_Paint(object sender, PaintEventArgs e)
+        {
+            _graphics = gamePanel.CreateGraphics();
+            DrawField();
+        }
+
+        enum SpaceTypes
+        {
+            Open,
+            X,
+            O,
+            Error
+        }
+
+        private void gamePanel_MouseClick(object sender, MouseEventArgs e)
+        {
+            int row = 4, column = 4;
+            if (e.X < 190)
+                row = 0;
+            if (e.X > 205 && e.X < 395)
+                row = 1;
+            if (e.X > 410)
+                row = 2;
+            if (e.Y < 190)
+                column = 0;
+            if (e.Y > 205 && e.Y < 395)
+                column = 1;
+            if (e.Y > 410)
+                column = 2;
+
+            if (row != 4 && column != 4)
+            {
+                _fieldSpaces[row, column] = SpaceTypes.O;
+                DrawField();
+            }
+        }
+    }
+}
